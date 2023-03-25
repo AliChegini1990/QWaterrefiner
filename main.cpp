12446992d3fd617fdf108cccd04947f3e580e115
alireza chegeni <ali.chegini.1990@gmail.com>
2023-03-25 13:30:32 +0330

    Add a description

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 // Programmer : alireza chegeni
 // Date = 2016
 // Email : ali.chegini.1990@gmail.com
+// Description: 
+//  تعویض فیلتر های مروبط به دستگاه تصفیه آب
 
 #include "mainwindow.h"
 #include <QApplication>
